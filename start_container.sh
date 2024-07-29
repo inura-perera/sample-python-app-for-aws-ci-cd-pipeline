dc5a7ce91d7aecb16fc84c220a4dfbcbddfb0dbf
Inura Perera <79038293+inura-perera@users.noreply.github.com>
2024-07-30 01:42:35 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull inura0924/simple-python-app
+docker pull inura0924/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 inura0924/simple-python-app
+docker run -d -p 5000:5000 inura0924/simple-python-flask-app
