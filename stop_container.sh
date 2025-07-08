7eabc0069d268df0eb4fb357c82e5b9d41581ace
Inura Perera <79038293+inura-perera@users.noreply.github.com>
2025-07-08 21:04:04 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker stop python-flask-app || true && docker rm python-flask-app || true
+set -e
+
+echo "Stopping container if running..."
+docker stop python-flask-app || true
+docker rm python-flask-app || true
+
