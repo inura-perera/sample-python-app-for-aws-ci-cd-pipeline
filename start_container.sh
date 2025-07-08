ab1bb563d23a0569d67f16551205a9d43eeff8a6
Inura Perera <79038293+inura-perera@users.noreply.github.com>
2025-07-08 22:17:34 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,14 +2,21 @@
 set -e
 
 # Set these as environment variables or inject from DevOps
-OCIR_URL=dxb.ocir.io/ax6enss1ld89/simple-python-flask-app
+OCIR_REGION="dxb.ocir.io"
+OCIR_NAMESPACE="ax6enss1ld89"
+OCIR_REPO="demo"  # Change 'demo' to your actual repo name if different
+OCIR_URL="$OCIR_REGION/$OCIR_NAMESPACE/$OCIR_REPO"
 
 echo "Logging in to OCIR..."
-echo "$OCIR_PASSWORD" | docker login phx.ocir.io -u "$OCIR_USERNAME" --password-stdin
+echo "jFF)WKAfyo>8Le8JH(kR" | docker login "$OCIR_REGION" -u "$OCIR_NAMESPACE/imasha.nimantha@onelexiicon.com" --password-stdin
 
 echo "Pulling Docker image..."
 docker pull "$OCIR_URL:latest"
 
+echo "Removing any existing container..."
+docker rm -f python-flask-app || true
+
 echo "Running container..."
 docker run -d --name python-flask-app -p 5000:5000 "$OCIR_URL:latest"
+
 
